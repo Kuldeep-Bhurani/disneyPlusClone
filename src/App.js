f648e0685606d17eb4d76039c0e03eabdd85ac2d
Kuldeep-Bhurani <bhuranikuldeep@gmail.com>
2021-11-11 20:45:49 +0530

    Created and Completed design of login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Detail from './components/Detail';
+import Login from './components/Login';
 import {
   BrowserRouter as Router,
   Routes,
@@ -15,8 +16,9 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/detail" element={<Detail />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
+          <Route path="/detail" element={<Detail />} />
         </Routes>
       </Router>
     </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,79 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Login() {
     return (
-        <div>
-
-        </div>
+        <Container>
+            <Content>
+                <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+                <Signup>Get All There</Signup>
+                <Description>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras commodo consectetur lobortis. Mauris scelerisque scelerisque tempus. Donec imperdiet cursus lacus, ac interdum enim sodales sit amet. In hac habitasse platea dictumst. Vestibulum in mauris turpis. Morbi nec leo dolor.</Description>
+                <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
+            </Content>
+        </Container>
     )
 }
 
 export default Login
+
+const Container = styled.div`
+    min-height: calc(100vh - 70px);
+    position: relative;
+    display: flex;
+    align-items: top;
+    justify-content: center;
+    &:before{
+        position: absolute;
+        content: "";
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        opacity: 0.6;
+        z-index: -1;
+        background: url("/images/login-background.jpg");
+        background-position: top;
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+`
+
+const Content = styled.div`
+    max-width: 650px;
+    padding: 80px 40px;
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+`
+
+const CTALogoOne = styled.img`
+`
+
+const Signup = styled.a`
+    width: 100%;
+    background: #0063e5;
+    font-weight: bold;
+    padding: 17px 0;
+    color: #f9f9f9;
+    border-radius: 4px;
+    cursor: pointer;
+    text-align: center;
+    font-size: 18px;
+    transition: all 250ms;
+    letter-spacing: 1.5px;
+    margin-top: 8px;
+    margin-bottom: 12px;
+    &:hover{
+        background: #0056c7;
+    }
+`
+
+const Description = styled.p`
+    font-size: 11px;
+    letter-spacing: 1.5px;
+    text-align: center;
+    line-height: 1.5;
+`
+
+const CTALogoTwo = styled.img`
+`
